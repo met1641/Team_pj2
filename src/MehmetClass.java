885923f4b4b21741169b717244bc8c6252e70c9a
msivass <mehmetsivass@gmail.com>
2023-01-15 10:53:32 -0500

    Mehmet page created

---
+++ src/MehmetClass.java
@@ -0,0 +1,15 @@
+public class MehmetClass {
+
+
+    public static void main(String[] args) {
+
+        /*
+                    SDLC & AGILE
+                1.  What is Software Testing?
+                    Process of executing a program or application with the intent of find software bugs using functional and automation tools
+                      Process of validating/verifying a software program/application
+                    Testers should test to break approach, not test to pass
+         */
+
+    }
+}
